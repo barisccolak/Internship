6782b7809dc5ded3880769595a9a89cd553a20ed
Baris Can Colak <cancolakbaris@gmail.com>
2023-10-04 14:59:47 +0200

    Black is installed.

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 def function1(numb):
     return numb + 1
 
-def function2 (num):
+
+def function2(num):
     return num - 1
 
-def function3 (num):
+
+def function3(num):
     return 0
 
-def function4 (num):
+
+def function4(num):
     return 0
 
-def function5 (num):
+
+def function5(num):
     return 0
 
-#abcdefgh
+
+# abcdefgh
--- testmodule/example.py
+++ testmodule/example.py
@@ -1,15 +1,16 @@
-def function_one (number):
+def function_one(number):
     """
     Parameters
     ----------
-    number 
+    number
         The first parameter.
-        
+
     Returns
     -------
     int
     """
     return number + 1
 
-def function_two (number):
+
+def function_two(number):
     return number + 2
