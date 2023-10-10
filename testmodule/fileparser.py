deb50e5aad658ac7e97b56dd78f50363bb42d395
Baris Can Colak <cancolakbaris@gmail.com>
2023-10-10 13:51:00 +0200

    Rule class is created, third rule is developing.

--- testmodule/fileparser.py
+++ testmodule/fileparser.py
@@ -1,4 +1,70 @@
-"""fileparser.py contains a class to test if given .JBI file is operable."""
+import os
+
+class Rule:
+    def __init__(self, group, number, logic):
+        self.group = group
+        self.number = number
+        self.logic = logic
+
+    def apply_rule(self, job_file, group, number):
+        self.logic(job_file, self.group, self.number)
+
+def check_A(job_file, group, number):
+    """Check if program starts with a comment line 
+    directly after the NOP statement. ("JBI-W", 1)"""
+    if not job_file.programlines[1].startswith("'---------------"):
+        print(f"{file_name} - {group}{number} - [{job_file.separator+2}]: Every program should start with a comment line directly after the NOP statement." 
+        )
+        
+
+def check_B(job_file, group, number):
+    """Test and allow the job if program command SETREG MREG# is listed unter FOLDERNAME TWINCAT_KOMMUNIKATION.("JBI-W", 2)"""
+    is_allowed_flag = False
+    counter = 0
+
+    for i in job_file.programlines:
+        if (
+            i.startswith("SETREG MREG#")
+            and job_file.foldername == "WINCAT_KOMMUNIKATION"
+
+        ):
+            is_allowed_flag = True
+            break
+        else: 
+            counter += 1
+########index of the error is not right.
+    if not is_allowed_flag:
+        print(f"{file_name} - {group}{number} {len(job_file.headlines) + counter} :The program command SETREG MREG# should only be allowed when the job is listed under FOLDERNAME TWINCAT_KOMMUNIKATION")
+
+
+def check_C(job_file, group, number):
+    """If the job is in the folder STANDARD or MAIN, the line SET USERFRAME n must be present, where n is any numerical value. The command SET USERFRAME must be executed before the command CALL JOB:TRIGGER ARGF"PROGRAMM_EIN" is called."""
+    set_flag1 = False
+    set_flag2 = False
+    index_username = 0
+    index_trigger = 0
+    
+    if self.foldername == "STANDARD" or self.foldername == "MAIN":
+        
+        for i in job_file.programlines:
+            if ( i.startswith("SET USERNAME n"):
+                set_flag1 = True
+                index_username = i
+            elif (i.startswith("CALL JOB:TRIGGER ARGF\"PROGRAMM_EIN\""):
+                set_flag2 = True
+                index_trigger = i
+        if (set_flag1 and set_flag2 == True) and index_username <= index_trigger:
+                
+                
+            
+
+
+
+
+
+
+
+##########################
 class JobFile:
     """Public class to define jobFile."""
     
@@ -16,6 +82,7 @@ class JobFile:
         self.save_foldername()
         self.rule_list()
 
+    
     def read_file(self):
         """Class method to read the file and print the content."""
         try:
@@ -31,58 +98,45 @@ class JobFile:
                         # add the lines before NOP into headlines    
 
         except FileNotFoundError:
-            raise (f"File not found: {self.file_path}")
+            raise FileNotFoundError(f"File not found: {self.file_path}")
         except Exception as e:
-            raise (e)
+            raise e
 
+    
     def save_name(self):
         """Filter the characters in the name line until ' ,' and save as name."""
         until = " "
         self.name = self.headlines[1]
         self.name = self.name[self.name.index(until) :]
         self.name = self.name.strip()  # delete the empty space
-        print("name :", self.name)
+        print("Name:", self.name)
 
+    
     def save_foldername(self):
         """Filter the characters in the foldername line
         until ' ,' and save as foldername."""
         until = " "
         self.foldername = self.headlines[2]
         self.foldername = self.foldername[self.foldername.index(until) :]
-        self.foldername = self.foldername.strip()  # delete the emtpy space
-        print("Foldername :", self.foldername)
+        self.foldername = self.foldername.strip()  # delete the empty space
+        print("Foldername:", self.foldername)
 
+    
     def rule_list(self):
         """Contains the rules."""
-        self.rule_post_NOP()
-        self.rule_setreg_mreg()
+        rules = [
+            Rule("JBI-W", 1, logic=check_A),
+            Rule("JBI-W", 2, logic=check_B),
+        ]
+        
+        for rule in rules: 
+            rule.apply_rule(self, rule.group, rule.number)
 
-
-    def rule_post_NOP(self):
-        """Check if program starts with a comment line 
-        directly after the NOP statement."""
-        if not self.programlines[1].startswith("'---------------"):
-            print(
-                "Every program should start with a comment line directly after the NOP statement."
-            )
-
-
-    def rule_setreg_mreg(self):
-        """Test and allow the job if program command SETREG MREG# is listed unter FOLDERNAME TWINCAT_KOMMUNIKATION."""
-        is_allowed_flag = False
-
-        for i in self.programlines:
-            if (
-                i.startswith("SETREG MREG#")
-                and self.foldername == "TWINCAT_KOMMUNIKATION"
-            ):
-                is_allowed_flag = True
-                break
-
-        if is_allowed_flag is False:
-            print("The program command SETREG MREG# should only be allowed when the job is listed unter FOLDERNAME TWINCAT_KOMMUNIKATION")
 
 
 if __name__ == "__main__":
+    head, tail = os.path.split("/mnt/scratch/bcolak/test.JBI")
     file_path = "/mnt/scratch/bcolak/test.JBI"
+    file_name = tail
     job = JobFile(file_path)
+
